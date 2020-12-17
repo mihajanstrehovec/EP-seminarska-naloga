23757522a437c1c02f6e300bdabb1db0086135f6
MuhamedAmi <mk1691@student.uni-lj.si>
2020-12-17 01:03:55 +0100

    api

--- e-shop/index.php
+++ e-shop/index.php
@@ -173,18 +173,16 @@ $urls = [
             adminController::seznamProdajalcev();
         }
     },
-<<<<<<< HEAD
     //REST API
     "api/artikli" => function () {
         eShopRestControler::index();
-=======
+    },
     "admin/spremeni-geslo" => function () {
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             adminController::spremeniGesloSubmit();
         } else {
             adminController::spremeniGeslo();
         }
->>>>>>> f12325b0837624a805f064f64668b2ef461b44a1
     }
 ];
 
