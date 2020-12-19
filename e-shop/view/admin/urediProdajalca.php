9b980efed85cb89d22ccc009e29ede325d17ddb6
mihajanstrehovec <miha.strehovec23@gmail.com>
2020-12-19 16:19:04 +0100

    admin koda urejena in dokumentirana

--- e-shop/view/admin/urediProdajalca.php
+++ e-shop/view/admin/urediProdajalca.php
@@ -8,8 +8,6 @@
     <div class ="row no-gutters justify-content-center">
         <div class ="col-lg-10" >
         
-       
-        
             <div class ="titleProfil" align ="center" style ="padding-top: 0.4vh;">
                 <h4 style ="font-weight: 600;">UREDI PRODAJALCA - ID: <?= $_GET["idProdajalca"]?></h4>
             </div>
@@ -17,60 +15,54 @@
             <div class ="profilVsebnik" style="padding: 7%;">
             
                 <form action = "<?= BASE_URL . "admin/prodajalec/uredi"?>" method = "POST">
-                <input type = "hidden" name = "idProdajalca" value = "<?= $_GET["idProdajalca"] ?>">
-                <div class ="form-group ">
-                        <label for = "gesloStranke">Sprememba gesla</label>
-                        
-                        <?php if($err ==  "Vnešeno geslo je napačno") :?>
-                            <div class = "form-row">
-                                <h7 style ="color: red;" > <?= $err ?> </h7>
+                    <input type = "hidden" name = "idProdajalca" value = "<?= $_GET["idProdajalca"] ?>">
+                    
+                    <div class ="form-group ">
+                            <label for = "gesloStranke">Sprememba gesla</label>
+                            
+                            <?php if($err ==  "Vnešeno geslo je napačno") :?>
+                                <div class = "form-row">
+                                    <h7 style ="color: red;" > <?= $err ?> </h7>
+                                </div>
+                            <?php endif;?>
+                            
+                            <div class = "col-lg-5">
+                                <input type = "password" class ="form-control" name = "trenutnoGeslo" id ="gesloStranke" placeholder = "Vpišite trenutno geslo">
                             </div>
-                        <?php endif;?>
+                            
                         
-                        <div class = "col-lg-5">
+                            
+                            <div class = "col-lg-5" style ="margin-top: 1vh;">
+                                <input type = "password" class = "form-control" name = "geslo" placeholder = "Vpišite novo geslo">
+                            </div>
 
-                            <input type = "password" class ="form-control" name = "trenutnoGeslo" id ="gesloStranke" placeholder = "Vpišite trenutno geslo">
-                        </div>
-                        
-                       
-                        
-                        <div class = "col-lg-5" style ="margin-top: 1vh;">
-                            <input type = "password" class = "form-control" name = "geslo" placeholder = "Vpišite novo geslo">
-                        </div>
-                        <button type = "submit" class ="btn my-4" 
-                        style ="background-color: rgba(89,145,144,1); color: white; ">
-                        Spremeni</button>
-                        
+                            <button type = "submit" class ="btn my-4" style ="background-color: rgba(89,145,144,1); color: white; ">
+                                Spremeni
+                            </button>
+                            
 
-                </div>
+                    </div>
 
                 
 
                 </form>
 
                 <form action = "" method = "POST">
-                Spremeni uporabniško ime:
+                    Spremeni uporabniško ime:
                 
                         <input type = "hidden" name = "idProdajalca" value = "<?= INPUT_GET ?>">
-                       
                             <div class = "col-lg-5">
-                        <input type = "text" class = "form-control" name = "uporabniskoIme"  placeholder = "Vpišite novo uporabniško ime" >
-                        </div>
+                                <input type = "text" class = "form-control" name = "uporabniskoIme"  placeholder = "Vpišite novo uporabniško ime" >
+                            </div>
 
                         <div class = "col-lg-2">
-                        <button type = "submit" class ="btn my-4" 
-                        style ="background-color: rgba(89,145,144,1); color: white; margin-top: 0px !important; ">
-                        Spremeni</button>
+                            <button type = "submit" class ="btn my-4" style ="background-color: rgba(89,145,144,1); color: white; margin-top: 0px !important; ">
+                                Spremeni
+                            </button>
                         </div>
-                
-
-                
-
+            
                 </form>
-        
             </div>
-
         </div>
-    </div>
-        
+    </div>       
 </div>
