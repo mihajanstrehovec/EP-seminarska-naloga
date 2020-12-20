be18511d23150b2d2654a4f573237d45f7498c7c
mihajanstrehovec <miha.strehovec23@gmail.com>
2020-12-20 01:28:48 +0100

    posodobljeni fonti!!!! PREVERI CSS FILE NEWSTYLE ZA NAVODILA NASTAVITVE

--- e-shop/index.php
+++ e-shop/index.php
@@ -14,6 +14,7 @@ define("BASE_URL", $_SERVER["SCRIPT_NAME"] . "/");
 define("IMAGES_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "static/images/");
 define("CSS_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "static/css/");
 define("JS_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "static/JS/");
+// define("FONT_URL", rtrim($_SERVER["SCRIPT_NAME"], "index.php") . "static/fonts/");
 
 $path = isset($_SERVER["PATH_INFO"]) ? trim($_SERVER["PATH_INFO"], "/") : "";
 
--- e-shop/view/layout.php
+++ e-shop/view/layout.php
@@ -6,7 +6,6 @@ require_once 'db_files/db_artikel.php';
 
 
 
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <link rel ="stylesheet" href ="<?= CSS_URL . "mainStyle.css" ?>">
 <link rel ="stylesheet" href ="<?= CSS_URL . "trgovinaStyle.css" ?>">
@@ -195,8 +194,15 @@ $_SESSION['price'] = array(100);
 <?php endif;?>
         <!--<?= var_dump($_SESSION); ?>-->
         
+
+        <!-- <canvas id="canvas1" style="border: 1px solid black; z-index: -10000; position: absolute; left:0; top:0;"></canvas>  -->
+
+
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src = "<?= JS_URL . "cart.js"?>"></script>
 <script src = "<?= JS_URL . "navBar.js"?>"></script>
+<script src = "<?= JS_URL . "scroll.js"?>"></script>
+<!-- <script src="scroll.js"></script>  -->
